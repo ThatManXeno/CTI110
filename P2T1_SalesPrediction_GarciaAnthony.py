9c242be971c96ce420012b2be1dc3c2ccf7b2e7b
ThatManXeno <36613184+ThatManXeno@users.noreply.github.com>
2018-02-18 21:36:03 -0500

    Add files via upload
    First submission, tutorial assignment

---
+++ P2T1_SalesPrediction_GarciaAnthony.py
@@ -0,0 +1,11 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Anthony Garcia
+# 02/14/18
+
+
+total_sales = float( input ("Enter the projected sales: "))
+
+profit = total_sales * 0.23
+
+print ("The profit is $", format (profit, ",.2f"))
